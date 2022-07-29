d5805ca4ff12c2956830c6605e9a555e9dacf1ca
carososa-s <sosasalgueroc@gmail.com>
2022-07-28 23:40:07 -0300

    actualización

--- statistics.js
+++ statistics.js
@@ -1,12 +1,11 @@
-let urlChamber = "https://api.propublica.org/congress/v1/117/house/members.json";
 
-// let urlChamber;
-// if (document.title == "Attendance: House") {
-//     urlChamber = "https://api.propublica.org/congress/v1/117/house/members.json"
-// }
-// if (document.title == "Senate") {
-//     urlChamber = "https://api.propublica.org/congress/v1/117/senate/members.json"
-// }
+let urlChamber;
+if (document.title == "Attendance: House" || document.title == "Party Loyalty: House") {
+    urlChamber = "https://api.propublica.org/congress/v1/117/house/members.json"
+}
+if (document.title == "Attendance: Senate" || document.title == "Party Loyalty: Senate") {
+    urlChamber = "https://api.propublica.org/congress/v1/117/senate/members.json"
+}
 
 const options = {
     method: "GET",
@@ -20,7 +19,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            lessEngaged: [],
+
             memberChamber: [],
             statistics: {
                 D: "Democrats",
@@ -38,13 +37,30 @@ createApp({
                 lessLoyal: [],
                 mostLoyal: [],
                 mostEngaged: [],
-                
+                lessEngaged: [],
             }
         }
     },
     methods: {
-        filter: function () {
-            this.lessEngaged = this.memberChamber.filter(member => member.missed_votes_pct >= 70)
+        filtered: function (typeOfVotes, boolean) {
+            let sorted = this.memberChamber.filter(member => member.total_votes).sort((a, b) => boolean ? a[typeOfVotes] - b[typeOfVotes] : b[typeOfVotes] - a[typeOfVotes]);/*.slice(0, (this.memberChamber.length * 0.1))*/
+            let tenPerCent = Math.ceil(this.memberChamber.length * 0.1);
+            let reference = sorted[tenPerCent - 1][typeOfVotes];
+            if (boolean) {
+                return sorted.filter(member => member[typeOfVotes] <= reference);
+            } else {
+                return  sorted.filter(member => member[typeOfVotes] >= reference);
+            }
+
+        },
+        average: function (numberOfMember, party) {
+            let sum = this.memberChamber.filter(member => member.votes_with_party_pct !== undefined).filter(member => member.party === party).map(member => member.votes_with_party_pct).reduce((acc, act) => acc + act, 0);
+    
+            if (sum) {
+                return (sum / numberOfMember).toFixed(2);
+            } else {
+                return 0
+            }
 
         }
     },
@@ -53,143 +69,46 @@ createApp({
             .then(res => res.json())
             .then(datos => {
                 this.memberChamber = Array.from(datos.results[0].members);
-                // this.statistics.lessEngaged = Array.from(datos.results[0].members);
-                // this.statistics.mostEngaged = Array.from(datos.results[0].members);
-                // this.statistics.lessLoyal = Array.from(datos.results[0].members);
-                // this.statistics.mostLoyal = Array.from(datos.results[0].members);
-                
+                this.statistics.numberOfAllMembers = this.memberChamber.length;
+                this.statistics.numberOfD = this.memberChamber.filter(member => member.party === "D").length
+                this.statistics.numberOfR = this.memberChamber.filter(member => member.party === "R").length
+                this.statistics.numberOfID = this.memberChamber.filter(member => member.party === "ID").length;
+                this.statistics.votesAllMembers = (this.memberChamber.filter(member => member.votes_with_party_pct !== undefined).map(member => member.votes_with_party_pct).reduce((acc, act) => acc + act) / this.statistics.numberOfAllMembers).toFixed(2);
+                this.statistics.votesD = this.average(this.statistics.numberOfD, "D");
+                this.statistics.votesR = this.average(this.statistics.numberOfR, "R");
+                this.statistics.votesID = this.average(this.statistics.numberOfID, "ID");
+                this.statistics.mostLoyal = this.filtered("votes_with_party_pct", false);
+                this.statistics.lessLoyal = this.filtered("votes_with_party_pct", true);
+                this.statistics.mostEngaged = this.filtered("missed_votes_pct", true);
+                this.statistics.lessEngaged = this.filtered("missed_votes_pct", false);
             })
             .catch(error => console.log(error));
-        // this.statistics.lessLoyal = this.filter("votes_with_party_pct", true);
-        // this.statistics.mostLoyal = this.filter("votes_with_party_pct", false);
-        // this.statistics.mostEngaged = this.filter("missed_votes_pct", true);
-        this.filter("missed_votes_pct", true);
-        console.log(this.lessEngaged)
-    }
+    },
+
 
 }).mount('#mainTwo');
 
 
-//Imprime las tablas correspondientes a cada chamber
-function printAllTables(documentTitleOne, documentTitleTwo, chamber) {
-    if (document.title === documentTitleOne || document.title === documentTitleTwo) {
-        party = partys(chamber);
-        makeStatistics(party, chamber);
-        tableGlance(tableOne, party);
-        printMembers("lessLoyal", "loyal", tableLessLoyal);
-        printMembers("mostLoyal", "loyal", tableMostLoyal);
-        printMembers("lessEngaged", "engaged", tableLessEngaged);
-        printMembers("mostEngaged", "engaged", tableMostEngaged);
-    }
-}
 
-//Crea los valores pedidos de estadisticas
-function makeStatistics(partys, members) {
-    for (i = 0; i < partys.length; i++) {
-        let membersParty = partyMembers(members, partys[i]);
-        statistics[`numberOf${partys[i]}`] = (numberMembers(membersParty)).toString();
-        statistics[`votes${partys[i]}`] = (averageOfVotes(statistics[`numberOf${partys[i]}`], membersParty)).toFixed(2);
-    }
-    statistics.lessLoyal = pctVotes(members, "votes_with_party_pct", true);
-    statistics.mostLoyal = pctVotes(members, "votes_with_party_pct", false);
-    statistics.mostEngaged = pctVotes(members, "missed_votes_pct", true);
-    statistics.lessEngaged = pctVotes(members, "missed_votes_pct", false);
-}
-
-//Creates an array with partys of a chamber and all members
-function partys(members) {
-    let partys = [];
-    members.map(member => {
-        if (!partys.includes(member.party)) {
-            partys.push(member.party);
-        }
-    })
-    partys.push("AllMembers")
-    return partys;
-}
-
-//Imprime la cantidad de miembros de cada partido y de todos los miembros y el porcentaje de voto de cada uno.
-function tableGlance(table, partys) {
-    let fragment = document.createDocumentFragment();
-    let tableBody = table.children[1];
-    for (i = 0; i < partys.length; i++) {
-        let tr = document.createElement("tr");
-        let tdParty = document.createElement("td");
-        tdParty.textContent = statistics[`${partys[i]}`];
-        let tdNumMembers = document.createElement("td");
-        tdNumMembers.textContent = statistics[`numberOf${partys[i]}`];
-        let tdVotes = document.createElement("td");
-        tdVotes.textContent = `${statistics[`votes${partys[i]}`]}%`;
-        tr.append(tdParty, tdNumMembers, tdVotes);
-        fragment.appendChild(tr);
-    }
-    tableBody.appendChild(fragment);
-}
-
-//Prints loyalty or attenddance
-function printMembers(mostOrLeast, loyalOrEngaged, table) {
-    let fragment = document.createDocumentFragment();
-    let tableBody;
-    if (table !== null) {
-        tableBody = table.children[1];
-        let votes = statistics[mostOrLeast];
-        votes.forEach(member => {
-            let tr = document.createElement("tr");
-            let tdName = document.createElement("td");
-            let middle_name;
-            if (member.middle_name) {
-                middle_name = member.middle_name;
-            } else {
-                middle_name = "";
-            }
-            tdName.textContent = `${member.first_name} ${middle_name} ${member.last_name}`
-            let tdNumPartyVotes = document.createElement("td");
-            let tdPctVotes = document.createElement("td");
-            if (loyalOrEngaged === "loyal") {
-                tdNumPartyVotes.textContent = member.total_votes;
-                tdPctVotes.textContent = `${member.votes_with_party_pct}%`;
-            } else if (loyalOrEngaged === "engaged") {
-                tdNumPartyVotes.textContent = member.missed_votes;
-                tdPctVotes.textContent = `${member.missed_votes_pct}%`;
-            }
-            tr.append(tdName, tdNumPartyVotes, tdPctVotes);
-            fragment.appendChild(tr);
-        })
-        tableBody.appendChild(fragment);
-    }
-}
-
-//Array with members of a party
-function partyMembers(members, p4rty) {
-    if (p4rty == "AllMembers") {
-        return members;
-    }
-    return members.filter(member => member.party === p4rty);
-}
 
 //Calcula el promedio de votos
-function averageOfVotes(numOfMembers, members) {
-    let votes = members.filter(member => member.votes_with_party_pct !== undefined).map(member => member.votes_with_party_pct);
-    let sum = votes.reduce((acc, act) => acc + act);
-    let avg = sum / parseInt(numOfMembers);
-    return avg;
-}
+// function averageOfVotes(numOfMembers, members) {
+//     let votes = members.filter(member => member.votes_with_party_pct !== undefined).map(member => member.votes_with_party_pct);
+//     let sum = votes.reduce((acc, act) => acc + act);
+//     let avg = sum / parseInt(numOfMembers);
+//     return avg;
+// }
 
-//Cuenta la cantidad de miembros
-function numberMembers(partyMembers) {
-    let numberMembers = partyMembers.length;
-    return numberMembers;
-}
 
 //Analiza el 10% de miembros que tienen la mayor o menor cantidad de un tipo de votos
-function pctVotes(members, typeOfVotes, boolean) {
-    let sorted = members.filter(member => member.total_votes).sort((a, b) => boolean ? a[typeOfVotes] - b[typeOfVotes] : b[typeOfVotes] - a[typeOfVotes]);
-    let tenPerCent = Math.ceil(numberMembers(members) * 0.10);
-    let reference = sorted[tenPerCent - 1][typeOfVotes];
-    if (boolean) {
-        return sorted.filter(member => member[typeOfVotes] <= reference);
-    } else {
-        return sorted.filter(member => member[typeOfVotes] >= reference);
-    }
-}
+// function pctVotes(members, typeOfVotes, boolean) {
+//     let sorted = members.filter(member => member.total_votes).sort((a, b) => boolean ? a[typeOfVotes] - b[typeOfVotes] : b[typeOfVotes] - a[typeOfVotes]);
+//     let tenPerCent = Math.ceil(numberMembers(members) * 0.10);
+//     let reference = sorted[tenPerCent - 1][typeOfVotes];
+//     if (boolean) {
+//         return sorted.filter(member => member[typeOfVotes] <= reference);
+//     } else {
+//         return sorted.filter(member => member[typeOfVotes] >= reference);
+//     }
+// }
 
